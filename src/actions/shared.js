2b2e881f70c62e86e533ac1e84b6281b150b6910
aleksandar <alpe88.dev@gmail.com>
2018-08-19 15:56:17 -0700

    streamlined dir structure

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,13 +1,30 @@
+//importing the initial data from our api.
+import { getInitialData } from '../utils/api'
+
+import { receiveUsers } from '../actions/users'
+import { recieveQuestions } from '../actions/questions'
+
+import { setAuthenticatedUser } from '../actions/authedUser'
 
 
+const testing = true
+const AUTHENTICATED_USER_ID = 'sarahedo'
 
 
-
+//we are exporting a function using the Redux Thunk function because we need to get data asyncronously.
 export function handleInitialData() {
 	return (dispatch) => {
     	return getInitialData()
+      		//this promise wil give us objects with users and questions properties and then add them to our store.
       		.then(({users, questions}) => {
-        
+        		//these dispatch calls still need reducers.
+          		dispatch(receiveUsers(users))
+          		dispatch(recieveQuestions(questions))
+          
+                if(testing) {
+                  dispatch(setAuthenticatedUser(AUTHENTICATED_USER_ID))
+                }
+          
         })
     }
 }
